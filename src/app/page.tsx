b268429e371032e39626b7e1312ae4f704413d21
amaitu <git@sgbarker.com>
2023-12-13 15:05:19 +0000

    Style start button and center it on the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                 <h1 className={'text-3xl text-center'}>Connection Check</h1>
             </div>
 
-            <div>
+            <div className={'m-auto flex justify-center mt-8'}>
                 <StartButton onClick={startCheck} shouldShow={!checkStarted}/>
             </div>
 
